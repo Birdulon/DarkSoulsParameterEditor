389e520f356e13c182889db9bbc68791b73bae2c
Luke Hubmayer-Werner <mongoose41@gmail.com>
2017-05-20 22:57:08 +0930

    Added struct definitions

--- DarkSoulsParameterEditor.py
+++ DarkSoulsParameterEditor.py
@@ -3,3 +3,66 @@ Documentation, License etc.
 
 @package DarkSoulsParameterEditor
 '''
+
+import sys
+import os
+from struct import unpack
+from itertools import chain
+from array import array
+
+
+pyqt_version = 0
+skip_pyqt5 = "PYQT4" in os.environ
+filename = "DaS memory.m0000"
+
+if not skip_pyqt5:
+    try:
+        from PyQt5 import QtGui, QtCore
+        from PyQt5.QtGui import QIcon, QPalette, QColor, QFont, QFontInfo, QImage, QPixmap
+        from PyQt5.QtWidgets import (
+            QApplication, QMainWindow, QFormLayout,
+            QGridLayout, QHBoxLayout, QVBoxLayout,
+            QAbstractItemView, QHeaderView, QListWidget,
+            QListWidgetItem, QTabWidget, QTableWidget,
+            QTableWidgetItem, QFrame, QScrollArea,
+            QStackedWidget, QWidget, QCheckBox, QComboBox,
+            QDoubleSpinBox, QGroupBox, QLineEdit,
+            QPushButton, QRadioButton, QSpinBox,
+            QStyleOptionButton, QToolButton, QProgressBar,
+            QDialog, QColorDialog, QDialogButtonBox,
+            QFileDialog, QInputDialog, QMessageBox,
+            QAction, QActionGroup, QLabel, QMenu, QStyle,
+            QSystemTrayIcon, QStyleOptionProgressBar
+        )
+        pyqt_version = 5
+    except ImportError:
+        print("Couldn't import Qt5 dependencies. "
+              "Make sure you installed the PyQt5 package.")
+if pyqt_version == 0:
+    try:
+        import sip
+        sip.setapi('QVariant', 2)
+        from PyQt4 import QtGui, QtCore
+        from PyQt4.QtGui import (
+            QApplication, QMainWindow, QFormLayout,
+            QGridLayout, QHBoxLayout, QVBoxLayout,
+            QAbstractItemView, QHeaderView, QListWidget,
+            QListWidgetItem, QTabWidget, QTableWidget,
+            QTableWidgetItem, QFrame, QScrollArea,
+            QStackedWidget, QWidget, QCheckBox,
+            QComboBox, QDoubleSpinBox, QGroupBox,
+            QLineEdit, QPushButton, QRadioButton,
+            QSpinBox, QStyleOptionButton, QToolButton,
+            QProgressBar, QDialog, QColorDialog,
+            QDialogButtonBox, QFileDialog, QInputDialog,
+            QMessageBox, QAction, QActionGroup,
+            QLabel, QMenu, QStyle,
+            QSystemTrayIcon, QIcon, QPalette, QColor,
+            QValidator, QFont, QFontInfo, QImage, QPixmap
+        )
+        from PyQt4.QtGui import QStyleOptionProgressBarV2 as QStyleOptionProgressBar
+        pyqt_version = 4
+    except ImportError:
+        print("Couldn't import Qt dependencies. "
+              "Make sure you installed the PyQt4 package.")
+        sys.exit(-1)
