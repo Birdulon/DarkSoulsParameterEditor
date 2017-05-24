4ba4202f874e611a708b68df0b42a409e2b5679f
Luke Hubmayer-Werner <mongoose41@gmail.com>
2017-05-24 19:40:20 +0930

    Changed to buffer interface, temporarily removed weapons view

--- DarkSoulsParameterEditor.py
+++ DarkSoulsParameterEditor.py
@@ -8,6 +8,8 @@ import sys
 import os
 import ctypes
 import struct
+import re
+import itertools
 
 import structs
 
@@ -83,82 +85,112 @@ class DarkSoulsParameterEditor(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self, None)
 
-        with open(filename, 'rb') as file1:
-            MEM = file1.read()
+        #with open(filename, 'rb') as file1:
+            #MEM = file1.read()
+        file1 = open(filename, 'rb')
+        MEM = file1.read()
+        MEMv = memoryview(MEM)
 
-        #items = make_string_img_list(0x111380, 9, 256)
         """
         We use some magic numbers here to find the string data.
         Horribly crude but it does the job.
         """
-        def mks(pattern, **kwargs):
-            return make_strings(MEM, b'\x00\x00\x01\x00' + pattern + b'\x01\x00\x00\x00', **kwargs)
-
-        string_lists = {
-            'Goods Names':         mks(b'\x60\x2F\x00\x00'),
-            'Weapon Names':        mks(b'\xF0\xD7\x06\x00'),
-            'Protector Names':     mks(b'\x24\x14\x01\x00'),
-            'Acc. Names':          mks(b'\xC8\x07\x00\x00'),
-            'Magic Names':         mks(b'\x2C\x10\x00\x00'),
-            'NPC Names':           mks(b'\x2C\x07\x00\x00'),
-            'Game Area Names':     mks(b'\x5C\x08\x00\x00'),
-            'Tooltips':            mks(b'\xB4\x4B\x00\x00'),
-            'Weapon Types':        mks(b'\xC8\xAA\x00\x00'),
-            'Acc. Tooltips':       mks(b'\x14\x0B\x00\x00'),
-            'Goods Desc.':         mks(b'\xEC\xAD\x01\x00', MAX_LEN=1024),
-            'Weapon Desc.':        mks(b'\x00\x4A\x09\x00', MAX_LEN=1024),
-            'Protector Desc.':     mks(b'\x88\x7E\x01\x00', MAX_LEN=1024),
-            'Acc. Desc.':          mks(b'\x6C\x48\x00\x00', MAX_LEN=1024),
-            'Magic Tooltips':      mks(b'\x1C\x19\x00\x00', MAX_LEN=1024),
-            'Magic Desc.':         mks(b'\xB0\x7A\x00\x00', MAX_LEN=1024),  # Missing in old memdump
-            'Subtitles':           mks(b'\xDC\xC8\x03\x00', MAX_LEN=1024),
-            'Signs':               mks(b'\x60\x35\x00\x00', MAX_LEN=1024),
-            'Intro Subs':          mks(b'\xCC\x0B\x00\x00', MAX_LEN=1024),
-            'UI Messages':         mks(b'\x48\x6B\x00\x00', MAX_LEN=1024),
-            'UI Labels':           mks(b'\xF4\x07\x00\x00', MAX_LEN=1024),
-            'Misc. Tooltips':      mks(b'\xA0\x89\x00\x00', MAX_LEN=1024),  # Missing in old memdump
-            'Character Creation':  mks(b'\x60\x56\x00\x00', MAX_LEN=1024),
-            'UI Messages 2':       mks(b'\x80\x27\x00\x00', MAX_LEN=1024),
-            'unk':                 mks(b'\x04\x47\x00\x00', MAX_LEN=1024),
-            'UI Labels 2':         mks(b'\x6C\x17\x00\x00', MAX_LEN=1024),
-            'Moon':                mks(b'\xD4\x0C\x00\x00', MAX_LEN=1024),
-            'unk1':                mks(b'\xE8\x02\x00\x00', MAX_LEN=1024),
-            'unk2':                mks(b'\x10\x01\x00\x00', MAX_LEN=1024),  # Missing in old memdump
-            'Main Menu':           mks(b'\xC0\x49\x00\x00', MAX_LEN=1024),  # Missing in old memdump
-            'Goods Desc.':         mks(b'\xEC\x48\x00\x00', MAX_LEN=1024),
-            'Messages DLC':        mks(b'\xA0\x1C\x00\x00', MAX_LEN=1024),
-            'unk3':                mks(b'\x84\x07\x00\x00', MAX_LEN=1024),  # Missing in old memdump
-            'unk4':                mks(b'\xD8\x04\x00\x00', MAX_LEN=1024),
-            'Subtitles DLC':       mks(b'\x24\x8E\x00\x00', MAX_LEN=1024),
-            'Magic Desc. DLC':     mks(b'\xC0\x1B\x00\x00', MAX_LEN=1024),
-            'Weapon Desc. DLC':    mks(b'\xCC\x76\x00\x00', MAX_LEN=1024),
-            'DLC Arena':           mks(b'\x0C\x0A\x00\x00', MAX_LEN=1024),
-            'Protector Desc. DLC': mks(b'\x84\x2F\x00\x00', MAX_LEN=1024),  # Missing in old memdump
-            'Acc. Desc. DLC':      mks(b'\xCC\x03\x00\x00', MAX_LEN=1024),
-            'Goods Tooltips DLC':  mks(b'\xF8\x04\x00\x00', MAX_LEN=1024),
-            'Goods Names DLC':     mks(b'\xA0\x03\x00\x00', MAX_LEN=1024),
-            'Acc. Tooltips DLC':   mks(b'\x94\x00\x00\x00', MAX_LEN=1024),
-            'Acc. Names DLC':      mks(b'\x58\x00\x00\x00', MAX_LEN=1024),
-            'Weapon Types DLC':    mks(b'\xC4\x12\x00\x00', MAX_LEN=1024),
-            'Weapon Names DLC':    mks(b'\xC0\x39\x00\x00', MAX_LEN=1024),
-            #'unk5':        mks(b'\xD4\x01\x00\x00', MAX_LEN=1024),  # Empty
-            'Protector Names DLC': mks(b'\x78\x12\x00\x00', MAX_LEN=1024),
-            'Magic Names DLC':     mks(b'\x18\x01\x00\x00', MAX_LEN=1024),
-            'Boss Names DLC':      mks(b'\x2C\x01\x00\x00', MAX_LEN=1024),
-            'Game Area Names DLC': mks(b'\x4C\x03\x00\x00', MAX_LEN=1024),
-            'unk6':                mks(b'\x54\x07\x00\x00', MAX_LEN=1024),
-            'unk7':                mks(b'\xA0\x07\x00\x00', MAX_LEN=1024),  # Missing in old memdump
-            'unk8':                mks(b'\x60\x07\x00\x00', MAX_LEN=1024),  # Missing in old memdump
+        strings_re = re.compile(b"\x00\x00\x01\x00(...)\x00\x01\x00\x00\x00")
+        string_list_keys = {
+            b'\x60\x2F\x00': 'Goods Names',
+            b'\xF0\xD7\x06': 'Weapon Names',
+            b'\x24\x14\x01': 'Protector Names',
+            b'\xC8\x07\x00': 'Acc. Names',
+            b'\x2C\x10\x00': 'Magic Names',
+            b'\x2C\x07\x00': 'NPC Names',
+            b'\x5C\x08\x00': 'Game Area Names',
+            b'\xB4\x4B\x00': 'Tooltips',
+            b'\xC8\xAA\x00': 'Weapon Types',
+            b'\x14\x0B\x00': 'Acc. Tooltips',
+            b'\xEC\xAD\x01': 'Goods Desc.',
+            b'\x00\x4A\x09': 'Weapon Desc.',
+            b'\x88\x7E\x01': 'Protector Desc.',
+            b'\x6C\x48\x00': 'Acc. Desc.',
+            b'\x1C\x19\x00': 'Magic Tooltips',
+            b'\xB0\x7A\x00': 'Magic Desc.',          # Missing in old memdump
+            b'\xDC\xC8\x03': 'Subtitles',
+            b'\x60\x35\x00': 'Signs',
+            b'\xCC\x0B\x00': 'Intro Subs',
+            b'\x48\x6B\x00': 'UI Messages',
+            b'\xF4\x07\x00': 'UI Labels',
+            b'\xA0\x89\x00': 'Misc. Tooltips',       # Missing in old memdump
+            b'\x60\x56\x00': 'Character Creation',
+            b'\x80\x27\x00': 'UI Messages 2',
+            b'\x04\x47\x00': 'unk',
+            b'\x6C\x17\x00': 'UI Labels 2',
+            b'\xD4\x0C\x00': 'Moon',
+            b'\xE8\x02\x00': 'unk1',
+            b'\x10\x01\x00': 'unk2',                 # Missing in old memdump
+            b'\xC0\x49\x00': 'Main Menu',            # Missing in old memdump
+            b'\xEC\x48\x00': 'Goods Desc.',
+            b'\xA0\x1C\x00': 'Messages DLC',
+            b'\x84\x07\x00': 'unk3',                 # Missing in old memdump
+            b'\xD8\x04\x00': 'unk4',
+            b'\x24\x8E\x00': 'Subtitles DLC',
+            b'\xC0\x1B\x00': 'Magic Desc. DLC',
+            b'\xCC\x76\x00': 'Weapon Desc. DLC',
+            b'\x0C\x0A\x00': 'DLC Arena',
+            b'\x84\x2F\x00': 'Protector Desc. DLC',  # Missing in old memdump
+            b'\xCC\x03\x00': 'Acc. Desc. DLC',
+            b'\xF8\x04\x00': 'Goods Tooltips DLC',
+            b'\xA0\x03\x00': 'Goods Names DLC',
+            b'\x94\x00\x00': 'Acc. Tooltips DLC',
+            b'\x58\x00\x00': 'Acc. Names DLC',
+            b'\xC4\x12\x00': 'Weapon Types DLC',
+            b'\xC0\x39\x00': 'Weapon Names DLC',
+            #'unk5':        mks(b'\xD4\x01\x00', MAX_LEN=1024),  # Empty
+            b'\x78\x12\x00': 'Protector Names DLC',
+            b'\x18\x01\x00': 'Magic Names DLC',
+            b'\x2C\x01\x00': 'Boss Names DLC',
+            b'\x4C\x03\x00': 'Game Area Names DLC',
+            b'\x54\x07\x00': 'unk6',
+            b'\xA0\x07\x00': 'unk7',                 # Missing in old memdump
+            b'\x60\x07\x00': 'unk8',                 # Missing in old memdump
         }
-        weapon_names = {k: v for (k, o, v) in string_lists['Weapon Names'] + string_lists['Weapon Names DLC']}
+        string_lists = {}
+        i = 0
+        while i < len(MEMv)-16:
+            match = strings_re.search(MEMv[i:])
+            if not match:
+                break
+            print('Strings match at 0x{:07x}'.format(match.start()+i))
+            if match.group(1) in string_list_keys:
+                result = make_strings(MEMv[i+match.start():])
+                string_lists[string_list_keys[match.group(1)]] = result[0]
+                i += result[1] + match.start()
+                print(result[1])
+            else:
+                i += match.end() + 2
+        print('String matches done')
 
-        weapons = make_weapons(MEM)
+        weapon_names = {k: v for (k, o, v) in itertools.chain(string_lists['Weapon Names'], string_lists['Weapon Names DLC'])}
+
+        #weapons = make_struct(MEMv, 'EQUIP_PARAM_WEAPON_ST', structs.EQUIP_PARAM_WEAPON_ST)
         param_lists = {}
-        for k, v in structs.structs.items():
-            try:
-                param_lists[k] = make_struct(MEM, k, v)
-            except BaseException as e:
-                print(e)
+        params_re = re.compile(b'|'.join(structs.structs.keys()))
+        print(params_re)
+        i = 0
+        while i < len(MEMv)-64:
+            match = params_re.search(MEMv[i:])
+            if not match:
+                break
+            print('Structs match at 0x{:07x} - {}'.format(match.start()+i, match.group(0)))
+            result = make_struct(MEMv[i+match.start()-2:], structs.structs[match.group(0)])
+            key = str(match.group(0), 'utf8').rstrip('\x00 ')
+            if key in param_lists:
+                j = 1
+                while '{} {:02}'.format(key, j) in param_lists:
+                    j += 1
+                key = '{} {:02}'.format(key, j)
+            param_lists[key] = result[0]
+            i += match.start() + result[1] - 2
+            print(result[1])
+        print('Struct matches done')
 
         str_headers = ['ID', 'Offset', 'String']
 
@@ -168,13 +200,13 @@ class DarkSoulsParameterEditor(QMainWindow):
         editor_tab = QTabWidget()
         self.tabwidget.addTab(structs_tab, "Structs")
         self.tabwidget.addTab(strings_tab, "Strings")
-        #self.tabwidget.addTab(editor_tab, "Editors")
+        self.tabwidget.addTab(editor_tab, "Editors")
 
-        structs_tab.addTab(make_param_table(weapons, IDs=weapon_names), "Weapons")
+        #structs_tab.addTab(make_param_table(weapons, IDs=weapon_names), "Weapons")
         for name, lst in sorted(param_lists.items()):
-            structs_tab.addTab(make_param_table(lst), name)
+            structs_tab.addTab(make_param_table(list(lst)), name)
         for name, lst in sorted(string_lists.items()):
-            strings_tab.addTab(make_table(str_headers, lst), name)
+            strings_tab.addTab(make_table(str_headers, list(lst)), name)
 
         layout = QHBoxLayout()
         layout.addWidget(self.tabwidget)
@@ -185,54 +217,32 @@ class DarkSoulsParameterEditor(QMainWindow):
         self.show()
 
 
-def make_weapons(memory):
+def make_struct(memory, struct_type):
     """
-    Weapon param structs
+    Try same as weapon param structs
     (u?)int16 holds number of entries
     After that, "EQUIP_PARAM_WEAPON_ST" in UTF-8
     14 bytes after null terminator, ID of first entry (uint32) followed by offset to struct (uint32) and 4 unk bytes (old name id?)
     repeat 12 byte struct for all entries
     """
-    idx = memory.find("EQUIP_PARAM_WEAPON_ST".encode('utf_8'))
-    if idx < 0:
-        raise ValueError('Weapon params not found.')
+    idx = 2
     num_structs = int.from_bytes(memory[idx-2:idx], 'little')
     start = idx + 0x24
     off_start = start + 12*num_structs
-    size = ctypes.sizeof(structs.EQUIP_PARAM_WEAPON_ST)
+    size = ctypes.sizeof(struct_type)
     ids = []
     for i in range(start, start+(12*num_structs), 12):
         ids.append(struct.unpack('III', memory[i:i+12]))
     params = []
-    for i in range(off_start, off_start+(size*num_structs), size):
-        params.append(structs.EQUIP_PARAM_WEAPON_ST.from_buffer_copy(memory[i:i+size]))
-    return list(zip(ids, params))
+    end = off_start+(size*num_structs)
+    for i in range(off_start, end, size):
+        params.append(struct_type.from_buffer_copy(memory[i:i+size]))
+    return zip(ids, params), end
 
 
-def make_struct(memory, string, cls):
-    """
-    Try same as weapon param structs
-    """
-    idx = memory.find(string.encode('utf_8'))
-    if idx < 0:
-        raise ValueError(string+' Params not found.')
-    num_structs = int.from_bytes(memory[idx-2:idx], 'little')
-    start = idx + 0x24
-    off_start = start + 12*num_structs
-    size = ctypes.sizeof(cls)
-    ids = []
-    for i in range(start, start+(12*num_structs), 12):
-        ids.append(struct.unpack('III', memory[i:i+12]))
-    params = []
-    for i in range(off_start, off_start+(size*num_structs), size):
-        params.append(cls.from_buffer_copy(memory[i:i+size]))
-    return list(zip(ids, params))
-
-
-def make_strings(memory, header, MAX_LEN=64):
-    idx = memory.find(header)
-    if idx < 0:
-        raise ValueError('Header not found.')
+def make_strings(memory, MAX_LEN=1024):
+    idx = 0  # Start of the memory passed is now the header
+    end_max = 16
     num_blocks = int.from_bytes(memory[idx+12:idx+16], 'little')
     IDs = []
     start = idx + 28
@@ -249,10 +259,11 @@ def make_strings(memory, header, MAX_LEN=64):
         end = s+2
         for i in range(s, s+MAX_LEN*2, 2):
             if int.from_bytes(memory[i:i+2], 'little') == 0:
-                end = i
+                end = i+2
                 break
-        strings.append(memory[s:end].decode('utf_16_le'))
-    return list(zip(IDs, str_offsets, strings))
+        strings.append(str(memory[s:end], 'utf_16_le'))
+        end_max = max(end, end_max)
+    return zip(IDs, str_offsets, strings), end_max
 
 
 def divceil(numerator, denominator):
@@ -339,6 +350,9 @@ def make_param_table(items, IDs=None, sortable=True, row_labels=True, scale=2):
     else:
         table.verticalHeader().setVisible(False)
     table.setHorizontalHeaderLabels(headers)
+    for i, title in enumerate(headers):
+        if title[:3] == 'pad' or title[:7] == 'reserve':
+            table.setColumnHidden(i, True)
     for row, item in enumerate(items):
         for col, val in enumerate(item[0]):
             q_item = QTableWidgetItem()
